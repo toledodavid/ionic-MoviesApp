4697b48fd8d724e0c5bf5588a67663fa40e24cf5
David Toledo <dtoledoz0207@gmail.com>
2019-11-23 13:30:50 -0600

    Create movie service
    This adds the movie service to add all the petitions to The Movie DB
    Server and add the HttpClientModule, algo this changes the icons to the tabs.

---
+++ src/app/services/movies.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+
+  constructor(private http: HttpClient) { }
+
+  getFeature() {
+    return this.http.get(`https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2019-01-01&primary_release_date.lte=2019-11-22&api_key=3f8ace2c02ff13704411f19cbfc2187d&language=es&include_image_language=es`);
+  }
+}
