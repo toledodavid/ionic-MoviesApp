8a87d8af83c29dd6de519fec988cbe1ac5dad9a6
David Toledo <dtoledoz0207@gmail.com>
2019-11-28 22:37:58 -0600

    Add-design-detalleComponent
    This adds the design to show the movie details and the actors, also
    add a footer in this component to close the modal of movie details.

--- src/app/components/detalle/detalle.component.ts
+++ src/app/components/detalle/detalle.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {MoviesService} from '../../services/movies.service';
+import {PeliculaDetalle, Cast} from '../../interfaces/interfaces';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-detalle',
@@ -9,18 +11,32 @@ import {MoviesService} from '../../services/movies.service';
 export class DetalleComponent implements OnInit {
 
   @Input() id;
+  movie: PeliculaDetalle;
+  actors: Cast[] = [];
+  hidden = 150;
+  slideOptActors = {
+    slidesPerView: 3.3,
+    freeMode: true,
+    spaceBetween: -5
+  };
 
-  constructor(private moviesService: MoviesService) { }
+  constructor(private moviesService: MoviesService, public modalCtrl: ModalController) { }
 
   ngOnInit() {
     //console.log('ID', this.id);
     this.moviesService.getMovieDetail(this.id).subscribe((response) => {
       console.log(response);
+      this.movie = response;
     });
 
     this.moviesService.getMovieActors(this.id).subscribe((response) => {
       console.log(response);
+      this.actors = response.cast;
     });
+  }
+
+  back() {
+    this.modalCtrl.dismiss();
   }
 
 }
