5201f1218e6c61479bf9119dfe2e54da0eda0f93
David Toledo <dtoledoz0207@gmail.com>
2019-12-02 12:16:23 -0600

    Add design for the movie posters in the tab2
    This adds the design for the posters when an user search a movie
    by title in the tab2 search page, and also add the ModalController
    to open the detail component with the movie detail. And also add
    a validation when nothing is the search input.

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import {MoviesService} from '../services/movies.service';
+import { Pelicula } from '../interfaces/interfaces';
+import { ModalController } from '@ionic/angular';
+import {DetalleComponent} from '../components/detalle/detalle.component';
 
 @Component({
   selector: 'app-tab2',
@@ -9,22 +12,44 @@ import {MoviesService} from '../services/movies.service';
 export class Tab2Page {
 
   searchText: string = '';
+  movies: Pelicula[] = [];
   ideas: string[] = ['Spiderman', 'Avenger', 'El señor de los anillos', 'La vida es bella'];
+  searching: boolean = false;
 
-  constructor(private moviesService: MoviesService) {}
+  constructor(private moviesService: MoviesService, private modalCtrl: ModalController) {}
 
 
   search(event) {
     const value = event.detail.value;
-    console.log(value);
-    this.moviesService.searchMovie(value).subscribe(response => {
-      console.log(response);
-    });
+
+    if(value) {
+      this.searching = true;
+      //console.log(value);
+      this.moviesService.searchMovie(value).subscribe(response => {
+        console.log(response);
+        this.movies = response['results'];
+        this.searching = false;
+      });
+    }else {
+      this.searching = false;
+      this.movies = [];
+    }
+    
   }
 
   saveIdea(idea: string) {
-    console.log(idea);
+    //console.log(idea);
     this.searchText = idea;
+  }
+
+  async showDetail(id: string) {
+    const modal = await this.modalCtrl.create({
+      component: DetalleComponent,
+      componentProps: {
+        id: id
+      }
+    });
+    await modal.present();
   }
 
 }
