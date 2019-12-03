aeff96dbc9bc097359453d3528cb11f2c0685340
David Toledo <dtoledoz0207@gmail.com>
2019-12-02 22:29:40 -0600

    prevent double information in the storage

--- src/app/services/data-local.service.ts
+++ src/app/services/data-local.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { PeliculaDetalle } from '../interfaces/interfaces';
+import { ToastController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,36 @@ export class DataLocalService {
 
   movies: PeliculaDetalle[] = [];
 
-  constructor(private storage: Storage) { }
+  constructor(private storage: Storage, public toastCtrl: ToastController) { }
 
   save_movie(movie: PeliculaDetalle) {
-    this.movies.push(movie);
+    let exist = false;
+    let message = '';
+
+    for(const movi of this.movies) {
+      if(movi.id === movie.id) {
+        exist = true;
+        break;
+      }
+    }
+
+    if(exist) {
+      this.movies = this.movies.filter(movi => movi.id !== movie.id);
+      message = 'Removido de favoritos';
+    }else {
+      this.movies.push(movie);
+      message = 'Agregada a favoritos';
+    }
+
+    this.presentToast(message);
     this.storage.set('movies', this.movies);
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastCtrl.create({
+      message: message,
+      duration: 1500
+    });
+    toast.present();
   }
 }
