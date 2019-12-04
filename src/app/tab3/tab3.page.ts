0419f3d5c4c3746b2f8030fcfe407f0604fae237
David Toledo <dtoledoz0207@gmail.com>
2019-12-03 18:41:48 -0600

    Create array to order movies by gender
    This adds a new function in tab3 to reorder the movies by gender and
    show it in the tab3Page.

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { PeliculaDetalle, Genre } from '../interfaces/interfaces';
 import {DataLocalService} from '../services/data-local.service';
 import { MoviesService } from '../services/movies.service';
@@ -8,16 +8,34 @@ import { MoviesService } from '../services/movies.service';
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page implements OnInit {
+export class Tab3Page {
 
   movies: PeliculaDetalle[] = [];
   genders: Genre[] = [];
 
+  favoriteByGender: any[] = [];
+
   constructor(private dataLocal: DataLocalService, private moviesService: MoviesService) {}
 
-  async ngOnInit() {
+
+  async ionViewWillEnter() {
     this.movies = await this.dataLocal.get_favorites();
     this.genders = await this.moviesService.getGenders();
+    this.moviesByGender(this.genders, this.movies);
+  }
+
+  moviesByGender(genders: Genre[], movies: PeliculaDetalle[]) {
+    this.favoriteByGender = [];
+    genders.forEach(gender => {
+      this.favoriteByGender.push({
+        gender: gender.name,
+        movis: movies.filter(movi => {
+          return movi.genres.find(genre => genre.id === gender.id);
+        })
+      });
+    });
+
+    console.log(this.favoriteByGender);
   }
 
 }
